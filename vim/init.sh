5c1d7833d97bf4a66d684d47a5e4fadfa713f41b
tomotana14 <tomotana14@gmail.com>
2017-04-26 16:33:47 +0900

    update

--- vim/init.sh
+++ vim/init.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
 cp vimrc ~/.vimrc
-mkdir -p ~/.vim/{bundles,snippet,skelton,rc}
+mkdir -p ~/.vim/{bundles,snippet,rc}
 cp ./dein.toml ~/.vim/rc/dein.toml
 cp ./dein_lazy.toml ~/.vim/rc/dein_lazy.toml
-cp ./skelton/skel.py ~/.vim/skelton/skel.py
 
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.vim/bundles
