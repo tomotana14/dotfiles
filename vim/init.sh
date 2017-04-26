0a4dea5f6c840fa0e222737590f8dcf014a6f687
tomotana14 <tomotana14@gmail.com>
2017-04-26 14:39:48 +0900

    update vim init.sh

--- vim/init.sh
+++ vim/init.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-mv vimrc ~/.vimrc
+cp vimrc ~/.vimrc
 mkdir -p ~/.vim/bundles
 mkdir ~/.vim/rc
-mv ./dein.toml ~/.vim/rc/dein.toml
+cp ./dein.toml ~/.vim/rc/dein.toml
 
 #install dein.vim
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
